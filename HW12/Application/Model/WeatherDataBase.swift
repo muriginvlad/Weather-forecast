c5f7f64260325768e7f02cd0b096857b4b7ce71c
Vladislav <muriginvlad@gmail.com>
2020-08-04 16:57:44 +0300

    Маленькие правки

--- HW12/Application/Model/WeatherDataBase.swift
+++ HW12/Application/Model/WeatherDataBase.swift
@@ -5,22 +5,22 @@
 //  Created by Владислав on 20.07.2020.
 //  Copyright © 2020 Murygin Vladislav. All rights reserved.
 //
-
-import Foundation
-
-class WeatherDataBase {
-    
-    static let shared = WeatherDataBase()
-
-    private let lastWeatherKey = "WeatherDataBase.lastWeatherKey"
-
-    var weather: [WeatherData] {
-        set { UserDefaults.standard.set(newValue, forKey: lastWeatherKey) }
-        get { return UserDefaults.standard.object(forKey: lastWeatherKey) as! [WeatherData]}
-    }
-
-
-    
+//
+//import Foundation
+//
+//class WeatherDataBase {
+//
+//    static let shared = WeatherDataBase()
+//
+//    private let lastWeatherKey = "WeatherDataBase.lastWeatherKey"
+//
+//    var weather: [WeatherData] {
+//        set { UserDefaults.standard.set(newValue, forKey: lastWeatherKey) }
+//        get { return UserDefaults.standard.object(forKey: lastWeatherKey) as! [WeatherData]}
+//    }
+//
+//
+//
 //    var weatherTemp: String? {
 //        set { UserDefaults.standard.set(newValue, forKey: lastWeatherKey) }
 //        get { return UserDefaults.standard.string(forKey: lastWeatherKey) }
@@ -31,7 +31,7 @@ class WeatherDataBase {
 //        set { UserDefaults.standard.set(newValue, forKey: lastCondition) }
 //        get { return UserDefaults.standard.string(forKey: lastCondition) }
 //    }
-
-}
+//
+//}
 
 
